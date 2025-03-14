7ed4317239c65aa113a57010e77be35148b964fe
MPBYUI <mpeel@byupathway.edu>
2025-03-13 20:26:58 -0600

    nav fix

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,12 @@
 // alert(document.lastModified);
 // // returns: Tuesday, December 16, 2017 11:09:42
 
-// var lastModified = document.lastModified;
-// document.getElementById("modified").innerHTML = lastModified;
+var lastModified = document.lastModified;
+document.getElementById("modified").innerHTML = lastModified;
 
 
 
 
-const date = new Date(2025,2,20,21,0,0);
+// const date = new Date(2025,2,20,21,0,0);
 
-console.log(date);
+// console.log(date);
