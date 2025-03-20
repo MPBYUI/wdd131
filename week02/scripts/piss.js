68c69ee18a866bbcd08d357a0918341a95f2c2a3
MPBYUI <mpeel@byupathway.edu>
2025-03-19 19:52:55 -0600

    . .

---
+++ week02/scripts/piss.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#fchapter');
+const deleteButton = document.createElement('button');
+deleteButton.textContent = '❌';
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+li.textContent = input.value;
+li.append(deleteButton);
+list.append(li);
+
+button.addEventListener('click', function() {
+    // Code to execute when the button is clicked
+});
